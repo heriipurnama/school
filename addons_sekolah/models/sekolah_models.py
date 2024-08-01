20a84ae5053f6faaf2ce0a7fee644944a0bc352c
heriipurnama <herii.sinatra@gmail.com>
2024-08-01 14:27:26 +0700

    feat: add name get

--- addons_sekolah/models/sekolah_models.py
+++ addons_sekolah/models/sekolah_models.py
@@ -1,9 +1,9 @@
-# File: models/sekolah_models.py
 from odoo import models, fields
 
 class Siswa(models.Model):
     _name = 'school.siswa'
     _description = 'Siswa'
+    _rec_name = 'nm_siswa'
 
     nis = fields.Char(string='NIS', required=True)
     nm_siswa = fields.Char(string='Nama Siswa', required=True)
@@ -15,19 +15,42 @@ class Siswa(models.Model):
     usia = fields.Integer(string='Usia')
     alamat = fields.Text(string='Alamat')
 
+    def name_get(self):
+        result = []
+        for record in self:
+            name = f"{record.nis} - {record.nm_siswa}"
+            result.append((record.id, name))
+        return result
+
 class Kelas(models.Model):
     _name = 'school.kelas'
     _description = 'Kelas'
+    _rec_name = 'nm_kelas'
 
     nm_kelas = fields.Char(string='Nama Kelas', required=True)
     nm_siswa_ids = fields.Many2many('school.siswa', string='Nama Siswa')
     wali_kelas = fields.Char(string='Wali Kelas')
 
+    def name_get(self):
+        result = []
+        for record in self:
+            name = record.nm_kelas
+            result.append((record.id, name))
+        return result
+
 class MataPelajaran(models.Model):
     _name = 'school.mata_pelajaran'
     _description = 'Mata Pelajaran'
+    _rec_name = 'nm_mata_pelajaran'
 
     nm_mata_pelajaran = fields.Char(string='Nama Mata Pelajaran', required=True)
+
+    def name_get(self):
+        result = []
+        for record in self:
+            name = record.nm_mata_pelajaran
+            result.append((record.id, name))
+        return result
 
 class Jadwal(models.Model):
     _name = 'school.jadwal'
@@ -38,9 +61,17 @@ class Jadwal(models.Model):
     jam = fields.Float(string='Jam', required=True)
     mata_pelajaran_id = fields.Many2one('school.mata_pelajaran', string='Mata Pelajaran', required=True)
 
+    def name_get(self):
+        result = []
+        for record in self:
+            name = f"{record.hari} - {record.kelas_id.nm_kelas} - {record.jam} - {record.mata_pelajaran_id.nm_mata_pelajaran}"
+            result.append((record.id, name))
+        return result
+
 class Guru(models.Model):
     _name = 'school.guru'
     _description = 'Guru'
+    _rec_name = 'nm_guru'
 
     nip = fields.Char(string='NIP', required=True)
     nm_guru = fields.Char(string='Nama Guru', required=True)
@@ -49,3 +80,10 @@ class Guru(models.Model):
     usia = fields.Integer(string='Usia')
     no_telp = fields.Char(string='No. Telp')
     alamat = fields.Text(string='Alamat')
+
+    def name_get(self):
+        result = []
+        for record in self:
+            name = f"{record.nip} - {record.nm_guru}"
+            result.append((record.id, name))
+        return result
